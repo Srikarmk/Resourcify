2d51058687f1f04e43eaab4226cf240aaf0ce2c0
Raj <blcc.raj@gmail.com>
2023-01-16 14:05:38 +0530

    replaced images source from png to svg

--- components/CodeCard.js
+++ components/CodeCard.js
@@ -4,30 +4,50 @@ const CodeCard = () => {
   return (
     <>
       <div className=" m-5 flex items-center justify-center h-[100px] w-[100px] shadow-xl bg-blue-50 rounded-lg">
-        <img src="../lang/js.png" alt="lang" className="h-[50%] lg:h-[70%]" />
-      </div>
-      <div className=" m-5 flex items-center justify-center h-[100px] w-[100px] shadow-xl bg-blue-50 rounded-lg">
-        <img src="../lang/cpp.png" alt="lang" className="h-[50%] lg:h-[70%]" />
-      </div>
-      <div className=" m-5 flex items-center justify-center h-[100px] w-[100px] shadow-xl bg-blue-50 rounded-lg">
-        <img src="../lang/java.png" alt="lang" className="h-[50%] lg:h-[70%]" />
-      </div>
-      <div className=" m-5 flex items-center justify-center h-[100px] w-[100px] shadow-xl bg-blue-50 rounded-lg">
-        <img src="../lang/py.png" alt="lang" className="h-[50%] lg:h-[70%]" />
-      </div>
-      <div className=" m-5 flex items-center justify-center h-[100px] w-[100px] shadow-xl bg-blue-50 rounded-lg">
         <img
-          src="../lang/kotlin.png"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
           alt="lang"
           className="h-[50%] lg:h-[70%]"
         />
       </div>
       <div className=" m-5 flex items-center justify-center h-[100px] w-[100px] shadow-xl bg-blue-50 rounded-lg">
-        <img src="../lang/ruby.png" alt="lang" className="h-[50%] lg:h-[70%]" />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"
+          alt="lang"
+          className="h-[50%] lg:h-[70%]"
+        />
       </div>
       <div className=" m-5 flex items-center justify-center h-[100px] w-[100px] shadow-xl bg-blue-50 rounded-lg">
         <img
-          src="../lang/swfit.png"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-plain.svg"
+          alt="lang"
+          className="h-[50%] lg:h-[70%]"
+        />
+      </div>
+      <div className=" m-5 flex items-center justify-center h-[100px] w-[100px] shadow-xl bg-blue-50 rounded-lg">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original-wordmark.svg"
+          alt="lang"
+          className="h-[50%] lg:h-[70%]"
+        />
+      </div>
+      <div className=" m-5 flex items-center justify-center h-[100px] w-[100px] shadow-xl bg-blue-50 rounded-lg">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg"
+          alt="lang"
+          className="h-[50%] lg:h-[70%]"
+        />
+      </div>
+      <div className=" m-5 flex items-center justify-center h-[100px] w-[100px] shadow-xl bg-blue-50 rounded-lg">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/swift/swift-original.svg"
+          alt="lang"
+          className="h-[50%] lg:h-[70%]"
+        />
+      </div>
+      <div className=" m-5 flex items-center justify-center h-[100px] w-[100px] shadow-xl bg-blue-50 rounded-lg">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg"
           alt="lang"
           className="h-[50%] lg:h-[70%]"
         />
